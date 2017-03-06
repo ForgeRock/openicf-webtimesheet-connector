9046c454becbee5c97d4be8e8cc45d8e6ee1973e
Jason Lemay <jason.lemay@forgerock.com>
2017-03-05 23:29:23 -0800

    OPENICF-667 Update license headers to the new forgerock license

--- src/test/config/org.forgerock.openicf.connectors.webtimesheet.WebTimeSheetConnector/config/config.groovy
+++ src/test/config/org.forgerock.openicf.connectors.webtimesheet.WebTimeSheetConnector/config/config.groovy
@@ -1,26 +1,9 @@
 /*
- * DO NOT REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright 2011-2017 ForgeRock AS. All Rights Reserved
  *
- * Copyright (c) 2011-2015 ForgeRock AS. All rights reserved.
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the License at
- * http://opensource.org/licenses/CDDL-1.0
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at http://opensource.org/licenses/CDDL-1.0
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
  */
 
 /* +---------------------------------------------------+
